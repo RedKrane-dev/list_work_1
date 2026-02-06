f54007a1825901203b3e85aed4b7d0d8fcafc0bb
RedKrane86 <redkrane86@gmail.com>
2026-02-07 00:31:35 +0500

    add try/except construction

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-fruits_list = ['Яблоко', 'Банан', 'Киви', 'Апельсин', 'Груша']
-
 def fruits_replacer(fruits):
-    fruits[0] = 'Персик'
-    fruits[-1] = 'Абрикос'
-    return fruits
+    try:
+        fruits[0], fruits[-1] = 'Персик', 'Абрикос'
+        return fruits
+
+    except IndexError:
+        return 'Передан пустой список'
+
+fruits_list = ['Яблоко', 'Банан', 'Киви', 'Апельсин', 'Груша']
 
 print(fruits_replacer(fruits_list))
